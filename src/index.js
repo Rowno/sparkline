5956d364d37916b2d96c2f8b4bdb6958ee199570
Roland Warmerdam <rowno@webspirited.com>
2017-06-25 23:35:04 -0700

    Use specific imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React, {PureComponent} from 'react'
 import PropTypes from 'prop-types'
-import * as d3Shape from 'd3-shape'
-import * as d3Scale from 'd3-scale'
+import {area, line, curveNatural} from 'd3-shape'
+import {scaleLinear} from 'd3-scale'
 import {max} from 'd3-array'
 
-const curve = d3Shape.curveNatural
+const curveFunction = curveNatural
 const defaultColors = {
   area: 'rgba(199, 228, 255, 0.5)',
   line: '#004585'
@@ -32,22 +32,22 @@ export default class SparklineD3 extends PureComponent {
     const {sparks, width, height} = this.props
     const maxX = max(sparks, s => s.values.length - 1)
     const maxY = max(sparks, s => max(s.values))
-    const x = d3Scale.scaleLinear().domain([0, maxX]).range([0, width])
+    const x = scaleLinear().domain([0, maxX]).range([0, width])
     // Set range 1 to make room for the line stroke
-    const y = d3Scale.scaleLinear().domain([0, maxY]).range([height, 1])
+    const y = scaleLinear().domain([0, maxY]).range([height, 1])
 
     const areaFunction =
-      d3Shape.area()
+      area()
       .x((d, i) => x(i))
       .y0(height)
       .y1(d => y(d))
-      .curve(curve)
+      .curve(curveFunction)
 
     const lineFunction =
-      d3Shape.line()
+      line()
       .x((d, i) => x(i))
       .y(d => y(d))
-      .curve(curve)
+      .curve(curveFunction)
 
     return (
       <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
