2ad579408c43e4b831608184f1aca15dd34cb2da
Roland Warmerdam <roland@segment.com>
2019-05-17 19:09:13 -0700

    Remove Makefile

--- src/index.test.js
+++ src/index.test.js
@@ -3,19 +3,11 @@ import React from 'react'
 import render from 'react-test-renderer'
 import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
-import Sparkline from '..'
+import Sparkline from '.'
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
-const values = [
-  354,
-  456,
-  200,
-  566,
-  344,
-  467,
-  545
-]
+const values = [354, 456, 200, 566, 344, 467, 545]
 const width = 56
 const height = 12
 
@@ -33,11 +25,7 @@ test('snapshot', t => {
 test('width', t => {
   const width = 123
   const component = (
-    <Sparkline
-      width={width}
-      height={height}
-      lines={[{values}]}
-    />
+    <Sparkline width={width} height={height} lines={[{values}]}/>
   )
   const wrapper = shallow(component)
   t.is(wrapper.prop('width'), width)
@@ -47,11 +35,7 @@ test('width', t => {
 test('height', t => {
   const height = 123
   const component = (
-    <Sparkline
-      width={width}
-      height={height}
-      lines={[{values}]}
-    />
+    <Sparkline width={width} height={height} lines={[{values}]}/>
   )
   const wrapper = shallow(component)
   t.is(wrapper.prop('height'), height)
@@ -61,11 +45,7 @@ test('height', t => {
 test('colors', t => {
   const colors = {area: '#e4f7ed', line: '#2B6B4C'}
   const component = (
-    <Sparkline
-      width={width}
-      height={height}
-      lines={[{values, colors}]}
-    />
+    <Sparkline width={width} height={height} lines={[{values, colors}]}/>
   )
   const paths = shallow(component).find('path')
   t.is(paths.first().prop('fill'), colors.area)
@@ -75,11 +55,7 @@ test('colors', t => {
 test('title', t => {
   const title = 'Line title'
   const component = (
-    <Sparkline
-      width={width}
-      height={height}
-      lines={[{values, title}]}
-    />
+    <Sparkline width={width} height={height} lines={[{values, title}]}/>
   )
   const wrapper = shallow(component)
   t.is(wrapper.find('path title').text(), title)
@@ -87,11 +63,7 @@ test('title', t => {
 
 test('multiple lines', t => {
   const component = (
-    <Sparkline
-      width={width}
-      height={height}
-      lines={[{values}, {values}]}
-    />
+    <Sparkline width={width} height={height} lines={[{values}, {values}]}/>
   )
   const wrapper = shallow(component)
   t.is(wrapper.find('g').length, 2)
